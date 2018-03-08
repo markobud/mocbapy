0568f2a541aaa6a4fc1f20e4bf121a9fa7e0d97d
Marko Budinich <marko.budinich@ls2n.fr>
2018-03-08 09:48:28 +0100

    MO-FVA implementation (optlang) & test file add

---
+++ mocbapy/test.py
@@ -0,0 +1,18 @@
+#mocbapy test file
+import cobra.test
+import mocbapy
+
+test_arr = list()
+n_test = 3
+for i in range(n_test):
+    model = cobra.test.create_test_model("ecoli")
+    model.id = model.id + str(i)
+    test_arr.append(model)
+
+common_mets = mocbapy.get_common_mets(test_arr)
+test_EcoSys = mocbapy.create_model(model_array=test_arr, metabolic_dict=common_mets)
+bensolve_opts = test_EcoSys.bensolve_default_options
+bensolve_opts['message_level'] = 0
+sol_mofba = mocbapy.mo_fba(test_EcoSys, options=bensolve_opts)
+sol_mofva = mocbapy.mo_fva(test_EcoSys, solver='gurobi')
+
